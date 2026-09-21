eef46f01c4fa68babed948f24a0ceee9d3602eef
Shivam Pal <shivampal1465@gmail.com>
2026-09-21 10:36:01 +0530

    Fix database configuration

---
+++ src/main/java/com/velinabliss/Velinabliss/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.velinabliss.Velinabliss.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://dawn-pine-150e.shivampal1465.workers.dev",
+                                "http://localhost:5500",
+                                "http://127.0.0.1:5500"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
